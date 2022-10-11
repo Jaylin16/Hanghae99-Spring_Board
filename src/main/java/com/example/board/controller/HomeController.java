cd47bb0b0d964139abaf4f8330f844527a917ad0
Jaylin16 <jaylin6115@gmail.com>
2022-10-12 02:08:01 +0900

    게시판 create.html 추가, spring security 수정 보완

---
+++ src/main/java/com/example/board/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.board.controller;
+
+import com.example.board.entity.model.UserRoleEnum;
+import com.example.board.security.UserDetailsImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        model.addAttribute("username", userDetails.getUsername());
+
+        if (userDetails.getUser().getRole() == UserRoleEnum.ADMIN) {
+            model.addAttribute("admin_role", true);
+        }
+
+        return "index";
+    }
+}
--- src/main/java/com/example/board/repository/UserRepository.java
+++ src/main/java/com/example/board/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.board.repository;
 
 import com.example.board.entity.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    
 }
